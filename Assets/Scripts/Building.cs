2bccdf9628d98d54b03050d718b07bd0ca08c6a2
Carl Larsson <cjohanlarsson@gmail.com>
2018-04-21 13:23:27 -0700

    best game eva - level 1

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour 
+{
+	[SerializeField] private string buildingGameId;
+	[SerializeField] private Transform basePosition;
+
+	public string BuildingGameId
+	{
+		get { return this.buildingGameId; }
+	}
+	public Vector3 BasePosition
+	{
+		get
+		{
+			return basePosition.position;
+		}
+	}
+}
---
+++ Assets/Scripts/LevelGoal.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGoal : MonoBehaviour 
+{
+	[SerializeField] SpriteRenderer goalIcon;
+	[SerializeField] string requiredBuildingGameId;
+	[SerializeField] float requiredDistToGoal = 0.5f;
+
+	Building attachedBuilding;
+
+	bool IsAttachedToBuilding
+	{
+		get
+		{
+			return attachedBuilding != null && 
+				attachedBuilding.gameObject != null &&
+				Vector3.Angle(Vector3.up, attachedBuilding.transform.up) < 5f &&
+				Vector3.Distance( attachedBuilding.BasePosition, this.transform.position ) < requiredDistToGoal ;
+		}
+	}
+
+	void Update()
+	{
+		if(IsAttachedToBuilding  )
+		{
+			goalIcon.color = Color.green;
+		}
+		else
+		{
+			goalIcon.color = Color.red;
+		}
+	}
+
+	void OnTriggerEnter(Collider c)
+	{
+		Debug.Log("entered" + c.gameObject.name.ToString());
+		var b = c.gameObject.GetComponent<Building>();
+		if(b != null && requiredBuildingGameId == b.BuildingGameId)
+		{
+			Debug.Log("building found!");
+			attachedBuilding = b;
+		}
+	}
+}
