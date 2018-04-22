05d76b2e01fccbb40b4165987e318e3c7b8dbaf8
Dan Colman <danrcolman@gmail.com>
2018-04-21 17:29:27 -0700

    DansDesign scene - People moving around and screaming and getting squashed.

---
+++ Assets/CitizenDeath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CitizenDeath : MonoBehaviour {
+
+    AudioSource squish;
+	private void Start()
+	{
+        squish = Camera.main.GetComponent<AudioSource>();
+	}
+	private void OnTriggerEnter(Collider other)
+	{
+        squish.Play();
+        //ADD CITIZEN--;
+        Destroy(transform.root.gameObject);
+
+	}
+}
