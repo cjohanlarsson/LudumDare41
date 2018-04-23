6986d75d524c0addf7155545480b21067fd263bd
Jon Pulsipher <jonapulse@gmail.com>
2018-04-22 21:47:09 -0700

    Game now captures cursor. Buttons have been replaced by keyboard commands to work with a captured cursor.

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -47,11 +47,39 @@ public class UI : MonoBehaviour
 
 	void Update()
 	{
+        if(Input.GetMouseButtonDown(0)) //Reset the cursor lockstate whenever player clicks
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
 		if(Level.current != null)
 		{
 			winScreen.SetActive( Level.current.CurrentState == Level.State.Won );
 			lossScreen.SetActive( Level.current.CurrentState == Level.State.Lost );
 		}
+
+        //Keyboard controls
+
+        if (Input.GetKeyDown(KeyCode.P))
+            TogglePause();
+        if(Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        if (paused) //Keyboard level select when paused
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+            {
+                SceneManager.LoadScene(1);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+            {
+                SceneManager.LoadScene(2);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
+            {
+                SceneManager.LoadScene(3);
+            }
+        }
 	}
 
     public void TogglePause()
